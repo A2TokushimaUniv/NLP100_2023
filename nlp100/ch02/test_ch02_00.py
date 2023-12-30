0ed7fbd784e9b4839a366ea6103c612cd3e7e2c1
Hayam112 <131961706+Hayam112@users.noreply.github.com>
2023-12-30 20:11:45 +0900

    行数取得の修正
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- nlp100/ch02/test_ch02_00.py
+++ nlp100/ch02/test_ch02_00.py
@@ -8,6 +8,6 @@ def test_行数のカウント():
 
     command = f"wc {file_path}"
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    line_num = result.stdout.split(" ")[1]
+    line_num = result.stdout.split()[0]
 
     assert len(df) == int(line_num)
